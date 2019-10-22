7626e594cd7cba88cc807637420c634680734f82
Lushenggang <lushg-tcxg@qq.com>
2019-10-22 21:15:35 +0800

    manage

--- middleware/manage.js
+++ middleware/manage.js
@@ -3,9 +3,9 @@ import { checkAdmin } from '@/api/user'
 export default ({ redirect }) => {
   checkAdmin().then(res => {
     if (res.status !== 200 || !res.data.admin) {
-      redirect('/site')
+      redirect('/')
     }
   }).catch(() => {
-    redirect('/site')
+    redirect('/')
   })
 }
