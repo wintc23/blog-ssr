0816f113d70c71dbf9868aeb1f96a7d01a3d1d54
Lushenggang <lushg-tcxg@qq.com>
2019-10-23 21:26:35 +0800

    push

--- push.sh
+++ push.sh
@@ -4,6 +4,7 @@ git push
 ssh root@wintc.top << autoscript
 cd /home/lushg/blog-ssr
 git pull origin master
+yarn
 yarn build
 lsof -i:8000 | grep 8000 | awk '{ print \$2 }' | xargs kill -9
 nohup yarn start &
