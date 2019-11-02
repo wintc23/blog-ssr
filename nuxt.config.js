8262859ec793ccace25da9ba056ae6af5257b43c
Lushenggang <lushg-tcxg@qq.com>
2019-11-02 22:44:50 +0800

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   head: {
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: "width=device-width, initial-scale=1" },
+      { name: 'viewport', content: "width=device-width, initial-scale=1, user-scalable=no" },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: "/favicon.ico" },
