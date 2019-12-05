ed98bbd86276b00fe6f6c6f6e36ac3c9b18335a4
Lushenggang <lushg-tcxg@qq.com>
2019-12-05 08:29:39 +0800

    push

--- layouts/default.vue
+++ layouts/default.vue
@@ -163,7 +163,7 @@ export default {
     },
     tags () {
       let list = this.$store.getters['site/tags'] || []
-      return list.filter(tag => !!tag.postCount).sort((a, b) => a.postCount - b.postCount)
+      return list.filter(tag => !!tag.postCount).sort((a, b) => b.postCount - a.postCount)
     },
     topics () {
       let list = this.$store.getters['site/topics'] || []
