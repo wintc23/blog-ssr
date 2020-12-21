1766e8e8068d497b7e95b0d7d021707f2e262b59
lushg <lushg-tcxg@qq.com>
2020-12-21 10:49:27 +0800

    editor

--- components/Editor.vue
+++ components/Editor.vue
@@ -26,6 +26,23 @@ import 'tinymce/plugins/codesample'
 import 'tinymce/plugins/advlist'
 import 'tinymce/plugins/link'
 
+let watermarkApi
+tinymce.PluginManager.add('watermark', function (editor) {
+  editor.ui.registry.getAll().icons.watermark || editor.ui.registry.addIcon('watermark', '<svg width="18" height="18" t="1608472079996" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3175" width="200" height="200"><path d="M439.003429 758.710857c45.860571 57.270857 105.179429 86.820571 175.104 86.820572 44.982857 0 72.265143-14.628571 115.419428-53.467429l12.214857-11.264c37.449143-35.035429 53.394286-44.982857 77.604572-44.982857 62.902857 0 80.457143 12.434286 147.382857 96.036571l57.051428-45.714285-22.601142-27.648c-64.365714-76.288-99.913143-95.817143-181.833143-95.817143-44.982857 0-72.265143 14.701714-115.419429 53.540571l-12.141714 11.190857c-37.522286 35.108571-53.394286 44.982857-77.677714 44.982857-47.323429 0-85.796571-19.163429-118.052572-59.392l-57.051428 45.714286zM439.003429 935.716571c45.860571 57.270857 105.179429 86.820571 175.104 86.820572 44.982857 0 72.265143-14.628571 115.419428-53.540572l12.214857-11.190857c37.449143-35.035429 53.394286-44.982857 77.604572-44.982857 62.902857 0 80.457143 12.434286 147.382857 96.036572l57.051428-45.787429-22.601142-27.574857c-64.365714-76.288-99.913143-95.817143-181.833143-95.817143-44.982857 0-72.265143 14.628571-115.419429 53.540571l-12.141714 11.190858c-37.522286 35.035429-53.394286 44.982857-77.677714 44.982857-47.323429 0-85.796571-19.163429-118.052572-59.465143l-57.051428 45.787428z" p-id="3176"></path><path d="M1024 0v585.142857h-73.142857V450.048l-208.896-177.005714-218.331429 363.812571-226.377143-150.966857L73.142857 671.012571V950.857143h292.571429v73.142857H0V0h1024z m-73.142857 73.142857H73.142857v517.412572l214.747429-198.729143 212.48 141.604571 220.525714-367.616L950.857143 373.028571V73.142857zM292.571429 146.285714a73.142857 73.142857 0 1 1 0 146.285715 73.142857 73.142857 0 0 1 0-146.285715z" p-id="3177"></path></svg>')
+  const options = {
+    tooltip: '图片添加水印',
+    icon: 'watermark',
+    onAction: (api) => {
+      api.setActive(!api.isActive());
+    },
+    onSetup (api) {
+      watermarkApi = api
+      api.setActive(true)
+    }
+  }
+  editor.ui.registry.addToggleButton('watermark', options)
+})
+
 // import ImagePreview from './ImagePreview'
 
 // tinymce.PluginManager.add('imagepreview', function (editor, url) {
@@ -79,8 +96,8 @@ export default {
         body_class: 'post-body',
         content_style: ' body { max-width: 1100px; min-height: 100%; margin: 0 auto; padding: 40px !important; background: #fff } html { background: #eee; height: 100% }',
         // menubar: 'edit view insert format tool table',
-        plugins: 'link lists image code table wordcount fullscreen media codesample advlist paste',
-        toolbar: 'formatselect indent outdent bullist numlist link image media table code codesample fullscreen | bold italic underline strikethrough | fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify',
+        plugins: 'link lists image code table wordcount fullscreen media codesample advlist paste watermark',
+        toolbar: 'formatselect indent outdent bullist numlist link image media table code codesample fullscreen | bold italic underline strikethrough | fontsizeselect | forecolor backcolor | alignleft aligncenter alignright alignjustify watermark',
         block_formats: 'H2=h2; H3=h3; H4=h4; P=p',
         menubar: false,
         file_picker_types: 'media',
@@ -106,7 +123,15 @@ export default {
         codesample_content_css: '/css/prism.css',
         paste_data_images: true,
         images_upload_handler: (blobInfo, success, failure) => {
+          function upload (blob) {
+            uploadFile(blob, url => {
+              success(url)
+            }, () => {
+              failure('上传失败，请重试')
+            })
+          }
           let blob = blobInfo.blob()
+          if (watermarkApi && !watermarkApi.isActive()) return upload(blob)
           let reader = new FileReader()
           reader.onload = e => {
             let img = new Image()
@@ -122,18 +147,11 @@ export default {
               ctx.fillStyle = '#FFC82C'
               ctx.textAlign = 'right'
               ctx.fillText('作者:木马啊', canvas.width - 10, canvas.height - fontSize - 10, canvas.width - 20)
-              this.watermark && ctx.fillText(this.watermark, canvas.width - 10, canvas.height - 10, canvas.width - 20)
+              ctx.fillText(this.watermark, canvas.width - 10, canvas.height - 10, canvas.width - 20)
               canvas.toBlob(upload)
             }
           }
           reader.readAsDataURL(blob)
-          function upload (blob) {
-            uploadFile(blob, url => {
-              success(url)
-            }, () => {
-              failure('上传失败，请重试')
-            })
-          }
         },
         file_picker_callback: (cb, value, meta) => {
           let input = document.createElement('input')
